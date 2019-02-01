ccbecf7ff2afecea54403398919ddf93d4d9bd22
jsin108 <jsin108@safeway.com>
2019-02-01 10:59:47 +0530

    Intial hue dockerfile

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -15,25 +15,3 @@ mount_adls_gen2() {
 
 mount_adls_gen2 ${ABFS_URI} ${AIRFLOW_HOME}
 
-case "$1" in
-  webserver)
-    airflow initdb
-    exec airflow webserver
-    ;;
-  worker|scheduler)
-    # To give the webserver time to run initdb.
-    sleep 10
-    exec airflow "$@"
-    ;;
-  flower)
-    sleep 10
-    exec airflow "$@"
-    ;;
-  version)
-    exec airflow "$@"
-    ;;
-  *)
-    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
-    exec "$@"
-    ;;
-esac
